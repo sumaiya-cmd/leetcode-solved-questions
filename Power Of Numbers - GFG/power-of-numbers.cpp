d333e00c5a48b8f8ed586ec3a0a47f9e56abc41a
sumaiya-cmd <88318661+sumaiya-cmd@users.noreply.github.com>
2023-07-12 17:51:09 +0530

    Added solution - LeetHub

---
+++ Power Of Numbers - GFG/power-of-numbers.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    //You need to complete this fucntion
+    int MOD =1e9+7 ;
+    long long power(int N,int R)
+    {
+       //Your code here
+       
+        long long ans = solve(N, R) ;
+        return ans%MOD ;
+    }
+    
+    long long solve(long long N, int R){
+        if(R==0)
+            return 1 ;
+        
+        if(R%2){
+            return ((N*solve((N*N)%MOD , R/2))%MOD)%MOD;
+        }    
+        
+        return solve((N*N)%MOD , R/2)%MOD ;
+    }
+};
+
+//{ Driver Code Starts.
+
+// compute reverse of a number 
+long long rev(long long n)
+{
+    long long rev_num = 0;
+     while(n > 0) 
+      { 
+        rev_num = rev_num*10 + n%10; 
+        n = n/10; 
+      } 
+      return rev_num;
+}
+
+
+
+
+int main()
+{
+    int T;
+    cin>>T;//testcases
+    
+    while(T--)
+    {
+        long long N;
+        cin>>N;//input N
+        
+        long long R = 0; 
+        
+        // reverse the given number n
+        R = rev(N);
+        Solution ob;
+        //power of the number to it's reverse
+        long long ans =ob.power(N,R);
+        cout << ans<<endl;
+    }
+}
+// } Driver Code Ends
